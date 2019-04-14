45d958c1b8f594cc194f8762788167609b664721
hada <nepturista@gmail.com>
2019-04-14 18:13:27 +0700

    satu

--- tubes/application/views/template/header.php
+++ tubes/application/views/template/header.php
@@ -8,41 +8,22 @@
     <title></title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-white">
-        <div class="container">
-            <div class="logo">
-                <a href="<?php echo base_url() ?>">
-                <img src="http://cdn1.tstatic.net/img/logo/tribun/svg/tribunnews.svg" alt="Tribun" height="40"></a>
-            </div>
-            <ul>
-                <li class="fbo2 grey ar" title="2019-04-10T22:32:09+07:00">Rabu, 10 April 2019</li>
-                <li <li class="havc search blue">
-                    <i class="fa fa-search fa-lg mr10"></i><span class="fbo2">Cari</span>   
-                    <div class="submenu">
-                        <div>
-                            <form action="http://www.tribunnews.com/search" id="cse-search-box">
-                                <input name="q" type="text" class="inputsearch" placeholder="Cari Berita" lang="id">
-                                <input type="hidden" name="cx" value="partner-pub-7486139053367666:4965051114">
-                                <input type="hidden" name="cof" value="FORID:10">
-                                <input type="hidden" name="ie" value="UTF-8">
-                                <input type="hidden" name="siteurl" value="www.tribunnews.com">
-                                <button type="submit" class="buttonsearch">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                                <div class="cl"></div>
-                            </form>
-                        </div>
-                    </div>
-                </li>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>">Home</a>
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
-                    <a class="nav-item nav-link" href="#">About</a>
+    <div class="navbar navbar-default" >
+        <div class="container-fluid" >
+            <div class="header" >
+                <div class="logo">
+                    <a href="<?php echo base_url() ?>" title="Tribun"><img src="http://cdn1.tstatic.net/img/logo/tribun/svg/tribunnews.svg" alt="Tribun" height="40"></a>
                 </div>
+                <ul>
+                    <li class="waktu">Minggu, 14 April 2019</li>
+                    <li class="cari">
+                        <i class="fa fa-search fa-lg mr10">
+                            ::before
+                        </i>
+                        <span calss="cari2">Cari</span>
+                        <div>
+                    </li>
+                </ul>
             </div>
         </div>
-    </nav> 
+    </div>
--- tubes/application/views/v_halutama.php
+++ tubes/application/views/v_halutama.php
@@ -5,12 +5,58 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 	<meta charset="utf-8">
-	<style type="text/css">
-	</style>
+	<link rel = "stylesheet" href = "<?php echo base_url() ?>assets/styleutama.css" type ="text/css">
 </head>
 <body>
-<div id="container">
-	<h1>Tes</h1>
+	<div id="container">
+		<div class="jarak"></div>
+		<div class ="badan" >
+			<div class="menubiru">
+				<div>
+					<ul>
+						<li style="border-left: none;">
+							<a href="">News</a>
+						</li>
+						<li>
+							<a href="">Bisnis</a>
+						</li>
+						<li>
+							<a href="">Superskor</a>
+						</li>
+						<li>
+							<a href="">Sport</a>
+						</li>
+						<li>
+							<a href="">Seleb</a>
+						</li>
+						<li>
+							<a href="">Lifestyle</a>
+						</li>
+						<li>
+							<a href="">Travel</a>
+						</li>
+						<li>
+							<a href="">Otomotif</a>
+						</li>
+						<li>
+							<a href="">Techno</a>
+						</li>
+						<li>
+							<a href="">Pilpres2019</a>
+						</li>
+						<li>
+							<a href="">Video</a>
+						</li>
+						<li>
+							<a href="">Images</a>
+						</li>
+						<li style="border-right:none;">
+							<a href="#">"Lainnya"<i class="fa fa-caret-down"></i>
+							</a>
+							<ul></ul>
+						</li>
+				</div>
+		</div>
 </div>
 </body>
 </html>
